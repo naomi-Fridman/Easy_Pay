b4f1b5290d88ff3d6b706182f79c37367e1ad7d6
naomi-Fridman <naomi@leader.codes>
2021-10-14 12:19:05 +0300

    validation ready

--- src/app/admin/deposites/new-deposite/new-deposite.component.ts
+++ src/app/admin/deposites/new-deposite/new-deposite.component.ts
@@ -13,12 +13,13 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class NewDepositeComponent implements OnInit {
   newDepositeDetailes: Depositor = new Depositor();
-  isExistUser:boolean=false;
-  constructor(private route:Router, private depositeService: DepositesService, private userService :UsersService) { }
+  isExistUser: boolean = false;
+  formSubmitAttempt: boolean = false;
+  constructor(private route: Router, private depositeService: DepositesService, private userService: UsersService) { }
   checkUserForDeposite(event) {
     this.userService.getUserByIdentityNumber(event.target.value).subscribe(user => {
       if (user) {
-        this.isExistUser=true;
+        this.isExistUser = true;
         this.DepositorDetailesUserForm.controls["id"].setValue(user.id);
         this.DepositorDetailesUserForm.controls["firstName"].setValue(user.firstName);
         this.DepositorDetailesUserForm.controls["lastName"].setValue(user.lastName);
@@ -32,34 +33,39 @@ export class NewDepositeComponent implements OnInit {
     })
   }
   save() {
-    if(this.isExistUser==true)
-    {
-      this.userService.updateUser(this.DepositorDetailesUserForm.value).subscribe(data=>{
-        this.DepositorDetailesDepositeForm.controls["userId"].setValue(data.id)
-        if(this.DepositorDetailesDepositeForm.controls["currencyId"].value=="3")
-        this.DepositorDetailesDepositeForm.controls["currencyId"].setValue(JSON.parse(this.DepositorDetailesDepositeForm.controls["currencyId"].value))
-        else if(this.DepositorDetailesDepositeForm.controls["currencyId"].value=="1") 
-        this.DepositorDetailesDepositeForm.controls["currencyId"].setValue(JSON.parse(this.DepositorDetailesDepositeForm.controls["currencyId"].value))
-        this.depositeService.postDeposite(this.DepositorDetailesDepositeForm.value).subscribe(e=>{
-          this.route.navigate(["/homePage"]);
-        });
-      })
+    this.formSubmitAttempt = true;
+    if (this.DepositorDetailesUserForm.valid && this.DepositorDetailesDepositeForm.valid) {
+      alert("valid")
+      if (this.isExistUser == true) {
+        this.userService.updateUser(this.DepositorDetailesUserForm.value).subscribe(data => {
+          this.DepositorDetailesDepositeForm.controls["userId"].setValue(data.id)
+          if (this.DepositorDetailesDepositeForm.controls["currencyId"].value == "3")
+            this.DepositorDetailesDepositeForm.controls["currencyId"].setValue(JSON.parse(this.DepositorDetailesDepositeForm.controls["currencyId"].value))
+          else if (this.DepositorDetailesDepositeForm.controls["currencyId"].value == "1")
+            this.DepositorDetailesDepositeForm.controls["currencyId"].setValue(JSON.parse(this.DepositorDetailesDepositeForm.controls["currencyId"].value))
+          this.depositeService.postDeposite(this.DepositorDetailesDepositeForm.value).subscribe(e => {
+            this.route.navigate(["/homePage"]);
+          });
+        })
+      }
+      else {
+        this.userService.postUser(this.DepositorDetailesUserForm.value).subscribe(data => {
+          this.DepositorDetailesDepositeForm.controls["userId"].setValue(data)
+          if (this.DepositorDetailesDepositeForm.controls["currencyId"].value == "3")
+            this.DepositorDetailesDepositeForm.controls["currencyId"].setValue(JSON.parse(this.DepositorDetailesDepositeForm.controls["currencyId"].value))
+          else if (this.DepositorDetailesDepositeForm.controls["currencyId"].value == "1")
+            this.DepositorDetailesDepositeForm.controls["currencyId"].setValue(JSON.parse(this.DepositorDetailesDepositeForm.controls["currencyId"].value))
+          this.depositeService.postDeposite(this.DepositorDetailesDepositeForm.value).subscribe(e => {
+            this.route.navigate(["/homePage"]);
+          });
+        })
+      }
     }
     else{
-      this.userService.postUser(this.DepositorDetailesUserForm.value).subscribe(data=>{
-        this.DepositorDetailesDepositeForm.controls["userId"].setValue(data)
-        if(this.DepositorDetailesDepositeForm.controls["currencyId"].value=="3")
-        this.DepositorDetailesDepositeForm.controls["currencyId"].setValue(JSON.parse(this.DepositorDetailesDepositeForm.controls["currencyId"].value))
-        else if(this.DepositorDetailesDepositeForm.controls["currencyId"].value=="1") 
-        this.DepositorDetailesDepositeForm.controls["currencyId"].setValue(JSON.parse(this.DepositorDetailesDepositeForm.controls["currencyId"].value))
-        this.depositeService.postDeposite(this.DepositorDetailesDepositeForm.value).subscribe(e=>{
-          this.route.navigate(["/homePage"]);
-        });
-      })
+      alert("not valid")
     }
-    
-      
-      //his.router.navigate(["/adminl"]);
+
+    //his.router.navigate(["/adminl"]);
     //}
   }
 
@@ -74,32 +80,32 @@ export class NewDepositeComponent implements OnInit {
     id: new FormControl(0),
     password: new FormControl(""),
     userName: new FormControl(""),
-    firstName: new FormControl("",{validators:[Validators.required],updateOn: 'blur'}),
-    lastName: new FormControl("" ,{validators:[Validators.required],updateOn: 'blur'}),
+    firstName: new FormControl("", { validators: [Validators.required], updateOn: 'blur' }),
+    lastName: new FormControl("", { validators: [Validators.required], updateOn: 'blur' }),
     address: new FormControl(""),
     city: new FormControl(""),
     telephoneNumber1: new FormControl(),
-    telephoneNumber2: new FormControl("",{validators:[Validators.required],updateOn: 'blur'}),
-    email: new FormControl("" ,{validators:[Validators.email],updateOn: 'blur'}),
+    telephoneNumber2: new FormControl("", { validators: [Validators.required], updateOn: 'blur' }),
+    email: new FormControl("", { validators: [Validators.email], updateOn: 'blur' }),
     comments: new FormControl(""),
-    identityNumber: new FormControl(null,{validators:[Validators.required],updateOn: 'blur'}),   
+    identityNumber: new FormControl(null, { validators: [Validators.required], updateOn: 'blur' }),
   });
 
   DepositorDetailesDepositeForm: FormGroup = new FormGroup({
-    
+
     id: new FormControl(0),
-    sum: new FormControl(null,{validators:[Validators.required],updateOn: 'blur'}),
-    currencyId: new FormControl(0,{validators:[Validators.required],updateOn: 'blur'}),
+    sum: new FormControl(null, { validators: [Validators.required], updateOn: 'blur' }),
+    currencyId: new FormControl(0, { validators: [Validators.required], updateOn: 'blur' }),
     returnDate: new FormControl(""),
-    comments : new FormControl(""),
+    comments: new FormControl(""),
     status: new FormControl(true),
     hebrewDepositeDate: new FormControl(""),
     hebrewDepositeReturnDate: new FormControl(""),
     // directDebitId: new FormControl(0),
-    paymentsNumber : new FormControl(0),
+    paymentsNumber: new FormControl(0),
     // creditCardId: new FormControl(0),
     userId: new FormControl(0),
-    depositeDate: new FormControl("",{validators:[Validators.required],updateOn: 'blur'}),
+    depositeDate: new FormControl("", { validators: [Validators.required], updateOn: 'blur' }),
     // currencyIdReturn: new FormControl(),
   });
 }
