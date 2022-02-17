2b8a66f24d339fa014f89b191897457b45e95d66
MiryamOrbach <miri.orbach23@gmail.com>
2022-02-17 20:55:07 +0200

    login and register

---
+++ src/app/models/MangerDetails.ts
@@ -0,0 +1,9 @@
+export class MangerDetails {
+  Id: number;
+  Password: string;
+  UserName: string;
+  FirstName: string;
+  LastName: string;
+  TelephoneNumber: string;
+  Email: string;
+}
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,21 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Loan } from '../models/Loan';
-import { User } from '../models/User';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Loan } from "../models/Loan";
+import { User } from "../models/User";
+import { MangerDetails } from "../models/MangerDetails";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-
 @Injectable()
 export class LoginService {
-  constructor(private _http: HttpClient) {
+  constructor(private _http: HttpClient) {}
+  getUserFromServer(
+    password: string,
+    userName: string
+  ): Observable<MangerDetails> {
+    return this._http.get<MangerDetails>(`/api/Login/${password}/${userName}`);
   }
-  getUserFromServer(code:string,accessName:string):Observable<User>{
-      return this._http.get<User>("/api/Login/"+code+"/"+accessName);
+
+  saveManagerInServer(manager: MangerDetails): Observable<number> {
+    return this._http.post<number>("/api/Login/addManager", manager);
   }
-  // addUser():Observable<
-  // saveStudentInServer(student:Student):Observable<void>{
-  //   return this._http.put<void>("/api/student",student)
-  // }
 }
